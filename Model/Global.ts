abcc11ca70260c4ee582efec20ef8e21b955f8ae
khanhtran <khanh.tran@firstpage.vn>
2022-03-11 10:12:27 +0700

    dispatch redux event

--- Model/Global.ts
+++ Model/Global.ts
@@ -1,74 +1,9 @@
-export type UserModel = {
-	PID: string;
-	FullName: string;
-	Picture: string;
-	platform: string;
-	RecentMsg: MessageModel;
-};
-
 export enum eRequestStatus {
 	REQUESTING = 'Requesting',
 	SUCCESS = 'Success',
 	FAILED = 'Failed',
 }
 
-export type MessageModel = {
-	mid: string;
-	text: string;
-	reply_to: string;
-	sender: string;
-	recipient: string;
-	time: number;
-	isRead: boolean;
-	isSended: boolean;
-	isPageMessage: boolean;
-	sendedStatus?: eRequestStatus;
-};
-
-export type SendMessageModel = {
-	messaging_type: string;
-	recipient: Recipient;
-	message: Message;
-};
-
-export type Recipient = {
-	id: string;
-};
-
-export type Message = {
-	mid?: string;
-	text: string;
-	quick_reply?: QuickReply;
-	reply_to?: ReplyTo;
-};
-
-export type QuickReply = {
-	payload: string;
-};
-
-export type ReplyTo = {
-	reply_to: string;
-};
-
-export enum eMessageType {
-	UPDATE = 'UPDATE',
-	RESPONSE = 'RESPONSE',
-	MESSAGE_TAG = 'MESSAGE_TAG',
+export interface tempModel {
+	tempProperty: string;
 }
-
-export enum ePlatform {
-	Facebook = 'Facebook',
-	Zalo = 'Zalo',
-}
-
-export type fbMsgApiResponse = {
-	recipient_id: string;
-	message_id: string;
-};
-
-export type CurrentChat = {
-	PID: string;
-	FullName: string;
-	ProfilePicture?: string;
-	Platform: string;
-};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,45 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import styles from '../styles/Home.module.css';
+import { setValue, selectExampleValue } from '../redux/Slices/exampleSlices';
+import { useAppDispatch, useAppSelector } from '../hooks/useRedux';
 
-const Home: NextPage = () => (
-	<div className={styles.container}>
-		<Head>
-			<title>React + Redux skeleton</title>
-			<meta name="description" content="React + Redux skeleton app" />
-			<link rel="icon" href="/favicon.ico" />
-		</Head>
-	</div>
-);
+const Home: NextPage = () => {
+	const dispatch = useAppDispatch();
+	const exampleValue = useAppSelector(selectExampleValue);
+
+	const randomExampleValue = () => {
+		dispatch(
+			setValue({
+				value: uuidv4(),
+			}),
+		);
+	};
+	return (
+		<div className={styles.main}>
+			<Head>
+				<title>React + Redux skeleton</title>
+				<meta name="description" content="React + Redux skeleton app" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div className={styles.grid}>
+				<h2 style={{ width: '100%', textAlign: 'center' }}>
+					{exampleValue.value}
+				</h2>
+				<button
+					className={styles.btnCustom}
+					type="button"
+					onClick={randomExampleValue}
+				>
+					Random Value
+				</button>
+			</div>
+		</div>
+	);
+};
 
 export default Home;
