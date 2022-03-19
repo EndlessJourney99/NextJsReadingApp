c6e0c3595478b25dc70f4fa6152d32f84e231024
khanhtran <khanh.tran@firstpage.vn>
2022-03-19 09:28:41 +0700

    update package

--- pages/[MetaUrl]/[BookId]/index.tsx
+++ pages/[MetaUrl]/[BookId]/index.tsx
@@ -1,14 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
-import {
-	Box,
-	Container,
-	Flex,
-	Heading,
-	Divider,
-	Center,
-} from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 
 import Layout from '../../../components/_shared/_layout';
